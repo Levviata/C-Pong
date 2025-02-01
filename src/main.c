3e00255bf8d1e0ccb327a7124b020947ee42756e
Levviata <68709348+Levviata@users.noreply.github.com>
2025-02-01 18:59:07 +0100

    Add AppState struct successfully

--- src/main.c
+++ src/main.c
@@ -11,22 +11,30 @@ static SDL_Vertex vert[vertLen];
 #define UPDATE_INTERVAL (1000 / 20) // Every 20 ticks (approx. every 50 ms)
 #define INPUT_INTERVAL (1000 / 30) // Every 30 ticks (approx. every 33.33 ms)
 
-static SDL_Window *window = NULL; // the window where our rendering takes place in (and game also, its a window duh)
-static SDL_Renderer *renderer = NULL; // the variable where we will mash shit-in to print a beautiful or disfigured picture (WHEN we actually tell the GPU to render it)
-
-static SDL_FRect left_player = {0,0,0,0};
-static SDL_FRect right_player = {0,0,0,0};
-
 static int windowWidth, windowHeight;
 
 static const char appName[] = "Levviata's C Based Pong";
 static const char version[] = "v0.0.0";
 static const char appIdentifier[] = "me.levviata.cpong";
 
+typedef struct {
+    SDL_Renderer* renderer;
+    SDL_Window *window;
+    SDL_FRect left_player;
+    SDL_FRect right_player;
+} AppState;
+
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    windowWidth = 800, windowHeight = 600;
+    AppState* state = malloc(sizeof(AppState));
+    if (!state) {
+        return SDL_APP_FAILURE; // Handle memory allocation failure
+    }
+
+    // nonsensical to set it here instead directly at creation but might change it later or do sum else so it stays like that
+    windowWidth = 800, windowHeight = 600; 
+
     // set our basic metadata, todo: expand later
     SDL_SetAppMetadata(appName, version, appIdentifier); 
         
@@ -38,11 +46,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     
     /* Create the window and the renderer! */
-    if (!SDL_CreateWindowAndRenderer(appName, windowWidth, windowHeight, false, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer(appName, windowWidth, windowHeight, false, &state->window, &state->renderer)) {
         SDL_Log("Couldn't create window/renderer: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
+    *appstate = state; // Set the pointer to the allocated state
     return SDL_APP_CONTINUE;
 }
 
@@ -56,30 +65,32 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     return SDL_APP_CONTINUE;
 }
 
-void render()
+void render(void *appstate)
 {
-    SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+    AppState* state = (AppState*)appstate; // Cast the void pointer back to AppState
 
-    left_player.h = 100;
-    left_player.w = left_player.h / 2;
-    left_player.x = ((windowWidth - left_player.w) / 2.0f); // Centering the rectangle
-    left_player.y = ((windowHeight - left_player.h) / 2.0f); // Centering the rectangle
+    SDL_GetWindowSize(state->window, &windowWidth, &windowHeight);
+
+    state->left_player.h = 100;
+    state->left_player.w = state->left_player.h / 2;
+    state->left_player.x = ((windowWidth - state->left_player.w) / 2.0f); // Centering the rectangle
+    state->left_player.y = ((windowHeight - state->left_player.h) / 2.0f); // Centering the rectangle
 
     // Clear the screen to black
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set black color
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255); // Set black color
+    SDL_RenderClear(state->renderer);
 
     // Set the draw color for the rectangle outline
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Set red color
+    SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, 255); // Set red color
 
-    SDL_RenderFillRect(renderer, &left_player);
+    SDL_RenderFillRect(state->renderer, &state->left_player);
 
     // Draw the rectangle outline
-    SDL_RenderRect(renderer, &left_player);
+    SDL_RenderRect(state->renderer, &state->left_player);
     
 
     // Present the rendered frame
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(state->renderer);
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
@@ -103,7 +114,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     }
 */
     // Render your graphics
-    render();
+    render(appstate);
 
     // Calculate frame time
     frameTime = SDL_GetTicks() - frameStart;
@@ -119,12 +130,13 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+    AppState* state = (AppState*)appstate;
     /* Seems like SDL destroys our render and window on app quit automatically?
     SDL_DestroyRenderer(renderer); // Destroy render 
     SDL_DestroyWindow(window); // Destroy window  */
 
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
 
-    //SDL_free(appstate);
+    SDL_free(appstate);
 }
 
