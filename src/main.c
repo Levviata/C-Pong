ac8c23c08802a468bf4f32eae5c91730f65a1815
Levviata <68709348+Levviata@users.noreply.github.com>
2025-02-01 22:48:30 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,18 +11,33 @@ static SDL_Vertex vert[vertLen];
 #define UPDATE_INTERVAL (1000 / 20) // Every 20 ticks (approx. every 50 ms)
 #define INPUT_INTERVAL (1000 / 30) // Every 30 ticks (approx. every 33.33 ms)
 
-static int windowWidth, windowHeight;
-
+// Metadata variables
 static const char appName[] = "Levviata's C Based Pong";
 static const char version[] = "v0.0.0";
 static const char appIdentifier[] = "me.levviata.cpong";
+
+static int windowWidth, windowHeight;
+
+static const int padding = 10;
+static const int speed = 1;
 
 typedef struct {
     SDL_Renderer* renderer;
     SDL_Window *window;
     SDL_FRect left_player;
-    SDL_FRect right_player;
-} AppState;
+    double yPlayer;
+    double yLastPlayer;
+    SDL_FRect right_ai_player;
+    double yAI;
+    double yLastAI;
+    float smoothing;
+} AppState; // this struct passes around ONLY variables that change during runtime (to not bloat the hardware's memory).
+
+double lerp(double varStart, double varEnd, float t) {
+    double result;
+    result = varStart + (varEnd - varStart) * t;
+    return result;
+}
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -34,6 +49,10 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
     // nonsensical to set it here instead directly at creation but might change it later or do sum else so it stays like that
     windowWidth = 800, windowHeight = 600; 
+    
+    state->yPlayer = 0, state->yAI = 0;
+
+    state->yPlayer = 0, state->yLastAI = 0;
 
     // set our basic metadata, todo: expand later
     SDL_SetAppMetadata(appName, version, appIdentifier); 
@@ -58,24 +77,69 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_KEY_DOWN ||
-        event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    AppState* state = (AppState*)appstate; // Cast the void pointer back to AppState
+
+    Uint32 frameStart = SDL_GetTicks();
+    int frameTime;
+
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; 
     }
+
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        switch (event->key.scancode) {
+            case SDL_SCANCODE_UP: // Arrow Up Key
+            case SDL_SCANCODE_W:  // 'W' key
+                state->yPlayer -= 5;
+                // e.g. state->player.y -= 5; // Move up
+                break;
+
+            case SDL_SCANCODE_DOWN: // Arrow Down Key
+            case SDL_SCANCODE_S:    // 'S' key
+                state->yPlayer += 5;
+                // e.g. state->player.y += 5; // Move down
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // Calculate frame time
+    frameTime = SDL_GetTicks() - frameStart;
+
+    // Delay to maintain 60 FPS
+    if (frameTime < FRAME_DELAY) {
+        SDL_Delay(FRAME_DELAY - frameTime);
+    }
+
+
     return SDL_APP_CONTINUE;
 }
 
-void render(void *appstate)
+void update(AppState *state) 
 {
-    AppState* state = (AppState*)appstate; // Cast the void pointer back to AppState
-
-    SDL_GetWindowSize(state->window, &windowWidth, &windowHeight);
+    SDL_GetWindowSize(state->window, &windowWidth, &windowHeight); // todo: if window is the same as last tick continue (so dont get the thing since it's size didnt change)
 
     state->left_player.h = 100;
-    state->left_player.w = state->left_player.h / 2;
-    state->left_player.x = ((windowWidth - state->left_player.w) / 2.0f); // Centering the rectangle
-    state->left_player.y = ((windowHeight - state->left_player.h) / 2.0f); // Centering the rectangle
+    state->left_player.w = state->left_player.h / 4; // make us a quarter in width compared to our height (so we are a slim rectangle)
+    state->left_player.x = padding;
+    state->left_player.y = ((windowHeight - state->left_player.h) / 2.0f) + state->yPlayer;
 
+    state->right_ai_player.h = 100;
+    state->right_ai_player.w = state->right_ai_player.h / 4; // make us a quarter in width compared to our height (so we are a slim rectangle)
+    state->right_ai_player.x = (windowWidth - state->right_ai_player.w) - padding;
+    state->right_ai_player.y = ((windowHeight - state->right_ai_player.h) / 2.0f) + state->yAI;
+
+    state->smoothing += speed;
+    if (state->smoothing > 1.0f) state->smoothing = 1.0f; // Clamp smoothing to avoid overflow
+
+    state->yLastPlayer = state->yPlayer;
+    state->yLastAI = state->yAI;
+}
+
+void render(AppState *state)
+{
     // Clear the screen to black
     SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255); // Set black color
     SDL_RenderClear(state->renderer);
@@ -83,38 +147,29 @@ void render(void *appstate)
     // Set the draw color for the rectangle outline
     SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, 255); // Set red color
 
+    // Fill our left player
     SDL_RenderFillRect(state->renderer, &state->left_player);
-
-    // Draw the rectangle outline
+    // Render our left player
     SDL_RenderRect(state->renderer, &state->left_player);
-    
+
+    // Fill our right player
+    SDL_RenderFillRect(state->renderer, &state->right_ai_player);
+    // Render our right player
+    SDL_RenderRect(state->renderer, &state->right_ai_player);
 
     // Present the rendered frame
     SDL_RenderPresent(state->renderer);
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    static Uint32 lastFrameTime = 0;
-    static Uint32 lastInputTime = 0;  // Time tracker for input
-    static Uint32 lastUpdateTime = 0; // Time tracker for updates
+    AppState* state = (AppState*)appstate; // Cast the void pointer back to AppState
 
     Uint32 frameStart = SDL_GetTicks();
     int frameTime;
-/*
-    // Process input every 30 ticks
-    if (frameStart - lastInputTime >= INPUT_INTERVAL) {
-        processInput();
-        lastInputTime = frameStart; // Update last input time
-    }
-/*
-    // Update every 20 ticks
-    if (frameStart - lastUpdateTime >= UPDATE_INTERVAL) {
-        update();
-        lastUpdateTime = frameStart; // Update last update time
-    }
-*/
+
     // Render your graphics
-    render(appstate);
+    update(state);
+    render(state);
 
     // Calculate frame time
     frameTime = SDL_GetTicks() - frameStart;
@@ -130,13 +185,13 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    AppState* state = (AppState*)appstate;
     /* Seems like SDL destroys our render and window on app quit automatically?
     SDL_DestroyRenderer(renderer); // Destroy render 
     SDL_DestroyWindow(window); // Destroy window  */
 
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
 
-    SDL_free(appstate);
+    AppState* state = (AppState*)appstate; // get the "appstate" pointer so that we can destroy it
+    SDL_free(appstate); // destroy appstate pointer
 }
 
